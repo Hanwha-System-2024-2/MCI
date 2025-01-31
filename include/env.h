b4c36a830816ccdbc2adba710863f13b33bea2c9
jinchandol <dlckswls111@gmail.com>
2025-01-31 08:02:43 +0000

    refactor: add padding

--- include/env.h
+++ include/env.h
@@ -4,10 +4,15 @@
 /****************************************************
     ec2 ip 사용 시, 보안을 위해 파일 .gitignore 추가하기
 ****************************************************/
+// #define OMS_SERVER_IP "106.101.9.158"
+// #define OMS_SERVER_PORT 8082
 
 #define OMS_SERVER_IP "127.0.0.1"
 #define OMS_SERVER_PORT 54321
 // IP : "43.201.73.157", PORT : 8080
+// #define KRX_SERVER_IP "43.201.73.157"
+// #define KRX_SERVER_PORT 8080
+
 #define KRX_SERVER_IP "127.0.0.1"
 #define KRX_SERVER_PORT 12345
 
